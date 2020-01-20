b7b1a579b6a98dea6f571eca6590a0a1a7fcade2
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-01-20 10:17:17 +0700

    First commit

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const localKey = 'dev-secret';
-const localAdress = 'mongodb://localhost:27017/mestodb';
+const localAdress = 'mongodb://localhost:27017/newsdb';
 module.exports = { localAdress, localKey };
