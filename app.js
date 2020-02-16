178bb45514ad3a08482f8485decd05e63e905d2d
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-16 14:30:41 +0700

    First commit

--- app.js
+++ app.js
@@ -40,7 +40,15 @@ const router = require('./routes/index.js');
 
 app.listen(PORT, () => {
 });
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'null');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 app.use(limiter);
 app.use(helmet());
 app.use(requestLogger);
