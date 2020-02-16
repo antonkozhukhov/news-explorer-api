13a03b0135b4f2a96ff76c3aeff46f93f59fa10c
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-16 10:20:33 +0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const articles = require('./articles');
 const users = require('./users');
@@ -8,15 +9,22 @@ const { login } = require('../controllers/login');
 const NotFoundError = require('../errors/not-found-error');
 const { resourceNotFoundMessage } = require('../messages');
 
+const corsOptions = {
+  origin: 'http:localhost:8081',
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+router.options('*', cors());
 router.use('/articles', auth, articles);
 router.use('/users', auth, users);
-router.post('/signin', celebrate({
+router.post('/signin', cors(corsOptions), celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
-router.post('/signup', celebrate({
+router.post('/signup', cors(corsOptions), celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
