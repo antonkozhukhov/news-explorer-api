97705c33ce2a25a724c6dcbc8ee6e1e2d87a18d0
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-16 13:25:40 +0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ const corsOptions = {
 router.options('*', cors());
 router.use('/articles', auth, articles);
 router.use('/users', auth, users);
-router.post('/signin', cors(corsOptions), celebrate({
+router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
