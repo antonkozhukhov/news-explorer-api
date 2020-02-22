3315ddbc9d4facb3de2828c9aa42ab9a22b2e41c
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-22 09:42:06 +0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,8 @@ router.use((req, res, next) => {
   if (whitelist.indexOf(req.headers.origin) !== -1) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   next();
