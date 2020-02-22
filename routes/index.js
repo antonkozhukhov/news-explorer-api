be7e350ac60a2b569ae6ce87eee71ad85335258e
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-22 18:54:23 +0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.use((req, res, next) => {
 
   next();
 }); */
-router.options('https://www.news-explorer.fun/index.html', cors());
+router.options('www.news-explorer.fun', cors());
 router.use('/articles', auth, articles);
 router.use('/users', auth, users);
 router.post('/signin', celebrate({
