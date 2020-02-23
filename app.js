8551a39d8c38e8fcc9ade0b7cceacf3ffb8a633d
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-23 10:45:28 +0700

    First commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Origin', 'https://www.news-explorer.fun');
   res.header('Access-Control-Allow-Headers', 'X-API-TOKEN, Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
+
 
 
   next();
