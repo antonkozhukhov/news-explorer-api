d137d3e8238549aebb2e051dfdcc46517d7c4116
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-23 09:56:24 +0700

    First commit

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const cenralizedError = require('./errors/centralized-error');
 
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', 'https://www.news-explorer.fun');
+  res.header('Access-Control-Allow-Headers', 'X-API-TOKEN, Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
+
+  next();
+});
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
