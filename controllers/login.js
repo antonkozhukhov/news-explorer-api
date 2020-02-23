0668b3b72349a724a7daa1912a2c00a257bb9713
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2020-02-23 13:39:20 +0700

    First commit

--- controllers/login.js
+++ controllers/login.js
@@ -22,7 +22,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
          sameSite: true,
       }); */
-      res.send(token);
+      res.send({ token });
     })
     .catch(() => {
       throw new AuthError(incorrectEmailOrParolMessage);
